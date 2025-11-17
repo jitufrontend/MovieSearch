eed3db7cbabf619bc70d6668b3a575221b829895
jitufrontend <jitendramistry500@gmail.com>
2025-11-17 10:51:32 +0530

    add url

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "./",  // 🔥 removes leading slash in build output
+  base: "", // 🔥 removes leading slash in build output
   plugins: [react()],
 });
